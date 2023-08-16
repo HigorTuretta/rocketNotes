731c88a40c94778cd31f4acf8ae2e5cb2449cfdb
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2023-08-16 19:34:00 -0300

    change api url

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,10 +3,15 @@ import { RiShutDownLine } from "react-icons/ri";
 import { useAuth } from "../../hooks/auth";
 import { api } from "../../services/api";
 import avatarPlaceHolder from "../../assets/avatar_placeholder.svg";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
   const { signOut, user } = useAuth();
-
+  const navigate = useNavigate();
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
   // montando o link para pegar a imagem de avatar
   const avatarUrl = user.avatar
     ? `${api.defaults.baseURL}/files/${user.avatar}`
@@ -20,7 +25,7 @@ export function Header() {
           <strong>{user.name}</strong>
         </div>
       </Profile>
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
